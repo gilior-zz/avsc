f1732f0b42768f17eadee5d90cf26bcd4c747326
liorg <QWE123>
2019-06-03 17:58:55 +0300

    more

--- src/bmd.js
+++ src/bmd.js
@@ -1,32 +1,94 @@
 // Write your code here
 
 class BMD {
+    constructor(path) {
+        this._path = path;
+    }
 
+    get path() {
+        return this._path;
+    }
+
+    set path(value) {
+        this._path = value;
+    }
 
     get_schema(schema_type) {
         return this[schema_type]()
     }
 
+    validate(schema_type, obj) {
+        var schame = this.get_schema(schema_type);
+
+        if (Object.entries(schame).length != Object.entries(obj).length) return false;
+        if (JSON.stringify(Object.keys(schame)) != JSON.stringify(Object.keys(obj))) return false;
+
+        var obj_entries = Object.entries(obj);
+
+        var schema_entries = Object.entries(schame);
+        for (var obj_entry of obj_entries) {
+            console.log('obj_value', obj_entry)
+
+
+            console.log('schema_entries', schema_entries)
+            // find on schema entry with this type
+            var entry = schema_entries.find(schema_entry => obj_entry[0] == schema_entry[0]);
+
+
+            console.log('entry', entry)
+            // if not found match entry on schema -> invalid obj param
+            if (!entry) return false;
+
+            //remove prop from schema
+            // delete schame[entry[0]];
+
+            console.log('schame', schame);
+
+
+        }
+        return true;
+
+    }
+
+    compareTypes(type_a, type_b) {
+        //primitve type
+        if (typeof type_a == 'string') {
+
+            // type not primitive
+            if (typeof type_b == 'string') return false
+        }
+        else  { // complex type
+
+        }
+
+    }
+
     level() {
-        console.log('hello')
+        return {
+            number: "number",
+            rooms: [{boundaryPoints: [{"x": "number", "y": "number"}]}],
+            architect: {"name": "string", "age": "number", "company": "string",}
+        }
     }
 
     line() {
         return {
-            "name": "line",
-            "fields": [{"name": "point", "type": "array", "items": "{\"name\": \"x\", \"type\": \"float\"}"}]
+            a: {x: "number", y: "number"},
+            b: {x: "number", y: "number"},
         }
     }
 
     point() {
         return {
-            "name": "point",
-            "fields": [{"name": "x", "type": "float"}, {"name": "y", "type": "float"}]
+            "x": "number", "y": "number"
         }
     }
 
     room() {
-        console.log('hello')
+        return {
+            number: "number",
+            boundaryPoints: [{x: "number", y: "number"}]
+        }
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 const argv = require('yargs').argv
 const bmd = require('./bmd')
-console.log(bmd.get_schema('line'));
-if (argv.ships > 3 && argv.distance < 53.5) {
+// console.log('point', bmd.get_schema('point'));
+// console.log("\n")
+// console.log('line', bmd.get_schema('line'));
+// console.log("\n")
+// console.log('room', bmd.get_schema('room'));
+// console.log("\n")
+// console.log('level', bmd.get_schema('level'));
+// console.log("\n")
 
-    console.log('Plunder more riffiwobbles!')
-} else {
-    console.log('Retreat from the xupptumblers!')
+
+//point
+obj = {x: 3, y: 7}
+isValid = bmd.validate('point', obj)
+console.log('point', isValid)
+
+
+//line
+obj = {
+    a: {x: 5, y: 6},
+    b: {x: 7, y: 8}
 }
+isValid = bmd.validate('line', obj)
+console.log('line', isValid)
+// bmd.validate('line', point) // false
+
+// if (argv.ships > 3 && argv.distance < 53.5) {
+//     console.log('Plunder more riffiwobbles!')
+// } else {
+//     console.log('Retreat from the xupptumblers!')
+// }
