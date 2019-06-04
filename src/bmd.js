ec38fdcde617ce470ab77662eb4218a56e8523a3
liorg <QWE123>
2019-06-04 15:08:18 +0300

    more

--- src/bmd.js
+++ src/bmd.js
@@ -17,42 +17,55 @@ class BMD {
         return this[schema_type]()
     }
 
-    validate(schema_type, obj) {
+    validate_obj(schema_type, obj) {
         var schame = this.get_schema(schema_type);
-
-
         return this.c(obj, schame)
+    }
+
+    validate(schema_type, schema_dict) {
+        for (var schema of schema_dict) {
+            var res = this.validate_obj(schema)
+            if (!res)
+                return false;
+        }
 
     }
 
     c(obj, schame) {
         var obj_entries = Object.entries(obj);
         var schema_entries = Object.entries(schame);
-        if (obj_entries.length != schema_entries.length) return false;
-        if (JSON.stringify(Object.keys(schame)) != JSON.stringify(Object.keys(obj))) return false;
         for (var obj_entry of obj_entries) {
-            console.log('obj_value', obj_entry)
 
 
-            console.log('schema_entries', schema_entries)
+
+
             // find on schema entry with this type
-            var schema_entry = schema_entries.find(schema_entry => obj_entry[0] == schema_entry[0]);
+            var schema_entry = schema_entries.find(schema_entry => (obj_entry[0] == schema_entry[0])
+                || (!isNaN(parseInt(obj_entry[0])) && !isNaN(parseInt(schema_entry[0]))));
 
 
-            console.log('schema_entry', schema_entry)
+
             // if not found match entry on schema -> invalid obj param
-            if (!schema_entry) return false;
+            if (!schema_entry)
+                return false;
             var is_same_type = this.compareTypes(schema_entry[1], obj_entry[1])
-            if (!is_same_type) return false;
+            if (!is_same_type)
+                return false;
         }
         return true;
     }
 
     compareTypes(schema_type, obj_value) {
+        if (Object.prototype.toString.call(schema_type) === '[object Object]')
+            if (Object.prototype.toString.call(obj_value) !== '[object Object]'
+                || Object.keys(schema_type).length != Object.keys(obj_value).length)
+                return false;
         //primitve type
         if (typeof schema_type == 'string') {
             var obj_type = typeof obj_value
-            return schema_type == obj_type
+            if (schema_type != obj_type)
+                return false;
+            return true;
         }
         else { // complex type
             return this.c(obj_value, schema_type)
@@ -63,7 +76,7 @@ class BMD {
     level() {
         return {
             number: "number",
-            rooms: [{boundaryPoints: [{"x": "number", "y": "number"}]}],
+            rooms: [{boundaryPoints: [{"x": "number", "y": "number"}], name: 'string', number: "number",}],
             architect: {"name": "string", "age": "number", "company": "string",}
         }
     }
@@ -83,6 +96,7 @@ class BMD {
 
     room() {
         return {
+            name: 'string',
             number: "number",
             boundaryPoints: [{x: "number", y: "number"}]
         }
